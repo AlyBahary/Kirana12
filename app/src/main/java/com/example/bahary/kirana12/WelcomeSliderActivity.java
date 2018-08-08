19a6acc20482b258ddfad46bf6a62e72df5fadc5
AlyBahary <Aly.Bahary.ab@gmail.com>
2018-08-08 23:43:54 +0200

    update

--- app/src/main/java/com/example/bahary/kirana12/WelcomeSliderActivity.java
+++ app/src/main/java/com/example/bahary/kirana12/WelcomeSliderActivity.java
@@ -17,39 +17,21 @@ public class WelcomeSliderActivity extends AppIntro {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Hawk.init(this).build();
-        //   Hawk.put(Constants.Welcome_Flag, "1");
-        Hawk.init(this).build();
-        if (!Hawk.contains(Constants.Welcome_Flag)) {
-            Intent i = new Intent(this, WelcomeSliderActivity.class);
-            startActivity(i);
-        }
+
+
+
         // AppIntro will do the rest.
-        addSlide(SampleSlide.newInstance(R.layout.slider1));
-        addSlide(SampleSlide.newInstance(R.layout.slider2));
+
+        SliderPage sliderPage=new SliderPage();
         //addSlide(LastSlider.newInstance(R.layout.slider3));
         LastSlider l=new LastSlider();
+        FirstSliderFragment f=new FirstSliderFragment();
+        SecondSliderFragment s = new SecondSliderFragment();
+        //addSlide(SampleSlide.newInstance(R.layout.slider1));
+        //addSlide(SampleSlide.newInstance(R.layout.slider2));
+        addSlide(f);
+        addSlide(s);
         addSlide(l);
- /*       SliderPage sliderPage = new SliderPage();
-        sliderPage.setTitle("");
-        sliderPage.setDescription("");
-        sliderPage.setImageDrawable(R.drawable.ic_remove_red_eye_black_24dp);
-
-        sliderPage.setBgColor(Color.parseColor("#3F51B5"));
-
-        addSlide(AppIntroFragment.newInstance(sliderPage));
-        SliderPage sliderPage1 = new SliderPage();
-        sliderPage1.setTitle("");
-        sliderPage1.setDescription("");
-        sliderPage1.setImageDrawable(R.drawable.ic_remove_red_eye_black_24dp);
-        sliderPage1.setBgColor(Color.parseColor("#3F51B5"));
-        addSlide(AppIntroFragment.newInstance(sliderPage1));
-
-        // OPTIONAL METHODS
-        // Override bar/separator color.
-        // setBarColor(Color.parseColor("#3F51B5"));
-        //setSeparatorColor(Color.parseColor("#2196F3"));
-*/
         // Hide Skip/Done button.
         setSeparatorColor(Color.parseColor("#fff2ca"));
         showSkipButton(false);
