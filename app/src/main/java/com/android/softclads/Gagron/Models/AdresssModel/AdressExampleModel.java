ad59ab409e5632407280fd22d12bf7c376119396
AlyBahary <Aly.bahary.ab@gmail.com>
2019-03-24 11:18:36 +0200

    commit

---
+++ app/src/main/java/com/android/softclads/Gagron/Models/AdresssModel/AdressExampleModel.java
@@ -0,0 +1,24 @@
+
+package com.android.softclads.Gagron.Models.AdresssModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class AdressExampleModel {
+
+    @SerializedName("addresses")
+    @Expose
+    private ArrayList<AddressModell> addresses = null;
+
+    public ArrayList<AddressModell> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(ArrayList<AddressModell> addresses) {
+        this.addresses = addresses;
+    }
+
+}
